129d59d04675adc247734ca7d7c30269428c65a8
Mordechai <mordwein77@gmail.com>
2023-09-07 14:31:27 -0400

    react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
+import NotFound from './NotFound';
 
 function App() {
   return (
     <div className="App">
-      <h1>Deploy Attempt</h1>
+        <Switch> 
+        <Route exact path="/" >
+          <Home />
+        </Route>
+        <Route path="*" >
+          <NotFound/>
+        </Route>
+     </Switch>
     </div>
   );
 }
