cd693248a30a8f82fbcdf534b1d825e0f7672839
Mordechai <mordwein77@gmail.com>
2023-09-10 11:28:37 -0400

    signin

--- client/src/NotFound.js
+++ client/src/NotFound.js
@@ -1,16 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { useHistory } from "react-router-dom";
+import Button from '@mui/material/Button';
 
 function NotFound() {
-
+    
     const history = useHistory()
 
-    useEffect(() => {
+    function handleClick() {
         history.push("/")
-    }, [])
-
+    }
+  
     return (
         <main style={{ minHeight: '100vh', display: 'flex'}}>
             <Container maxWidth="xl" 
@@ -24,10 +25,28 @@ function NotFound() {
                 <img width="85" height="85" src="https://img.icons8.com/glyph-neue/64/1f699d/goal--v1.png" alt="goal--v1"/>
                 <Typography variant="h2" sx={{color: '#1F699D', fontFamily: 'Merriweather Sans', paddingTop: '1.5rem'}}>JOBIFY</Typography>
                 <Typography variant="h7" sx={{color: '#1F699D', fontFamily: 'Merriweather Sans'}}>Unlock Your Career Potential</Typography>
-                <Typography variant="h3" sx={{color: '#1F699D', paddingTop: '1rem', fontFamily: 'Merriweather Sans'}}>Sorry, we couldn't find the page you are looking for.</Typography>
+                <Typography variant="h3" sx={{color: '#1F699D', paddingTop: '1rem', fontFamily: 'Merriweather Sans', textAlign: 'center'}}>Sorry, we couldn't find the page you are looking for.</Typography>
                 <Typography variant="h4" sx={{color: '#1F699D', paddingTop: '1rem', textAlign: 'center', fontFamily: 'Merriweather Sans'}}>
-                    Please navigate back to the previous page.
+                    Click the button below to navigate back to the home page.
                 </Typography>
+                <Button
+                    onClick={handleClick}
+                    variant="contained"
+                    sx={{
+                        marginTop: '2rem',
+                        fontWeight: 'bold',
+                        backgroundColor: '#1F699D',
+                        height: '3rem',
+                        borderRadius: '25px',
+                        fontSize: '1rem',
+                        fontFamily: 'Merriweather Sans',
+                        '&:hover': {
+                            backgroundColor: '#1F699D', 
+                          },
+                    }}
+                >
+                    Back to Home Page
+                </Button>
             </Container>
         </main>
        
