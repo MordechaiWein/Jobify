7b90fbee838812133182589f68509c9b723c1118
Mordechai <mordwein77@gmail.com>
2023-09-10 00:05:39 -0400

    try

--- client/src/NotFound.js
+++ client/src/NotFound.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import LogoImage from './Images/logo.png'
 
 function NotFound() {
-
-    const logo = LogoImage
 
     return (
         <main style={{ minHeight: '100vh', display: 'flex'}}>
@@ -17,7 +14,6 @@ function NotFound() {
                     alignItems: 'center'
                 }}
             >
-                <img style={{width: '8rem'}} src={logo}/> 
                 <Typography variant="h2" sx={{color: '#1F699D', fontFamily: 'Merriweather Sans', paddingTop: '1.5rem'}}>JOBIFY</Typography>
                 <Typography variant="h7" sx={{color: '#1F699D', fontFamily: 'Merriweather Sans'}}>Unlock Your Career Potential</Typography>
                 <Typography variant="h3" sx={{color: '#1F699D', paddingTop: '1rem', fontFamily: 'Merriweather Sans'}}>Sorry, we couldn't find the page you are looking for.</Typography>
--- client/src/SignIn.js
+++ client/src/SignIn.js
@@ -64,7 +64,7 @@ export default function SignIn() {
               required
               fullWidth
               id="email"
-              label="Email Address"
+            
               name="email"
               autoComplete="email"
               autoFocus
@@ -74,15 +74,12 @@ export default function SignIn() {
               required
               fullWidth
               name="password"
-              label="Password"
+             
               type="password"
               id="password"
               autoComplete="current-password"
             />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
+          
             <Button
               type="submit"
               fullWidth
@@ -91,18 +88,6 @@ export default function SignIn() {
             >
               Sign In
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
           </Box>
         </Box>
         <Copyright sx={{ mt: 8, mb: 4 }} />
