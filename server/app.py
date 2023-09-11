c506800bf38cc8f750a839c52f0c8dceda12f6ae
Mordechai <mordwein77@gmail.com>
2023-09-11 17:06:38 -0400

    big fix

--- server/app.py
+++ server/app.py
@@ -1,10 +1,9 @@
-import os
 from flask import jsonify, request, session, render_template
 from sqlalchemy.exc import IntegrityError
 from flask_restful import Resource
 from config import app, db, api, bcrypt
 from models import db, User, Job, Responsibility, Qualification
-print(os.environ.get("SESSION_KEY"))
+
 @app.errorhandler(404)
 def not_found(e):
     return render_template("index.html")
@@ -57,7 +56,7 @@ class Signup(Resource):
         email_address = request_json.get('email_address')
         
         user = User(
-            username=username,
+            username = username,
             email = email_address,
             admin = False
         )
@@ -103,5 +102,5 @@ api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(Logout, '/logout', endpoint='logout')
 
 
-# if __name__ == "__main__":
-#     app.run(port=8000, debug=True)
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
